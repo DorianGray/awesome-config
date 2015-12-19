7054e0cc86de74c6a0de4c043c95ca0baa89a379
Robert Andrew Ditthardt <drew.ditthardt@gmail.com>
2015-12-18 16:47:52 -0800

    rule cleanup

--- rules.lua
+++ rules.lua
@@ -4,27 +4,34 @@ local naughty = require 'naughty'
 
 local tags = require 'tags'
 
+local systray = 16
 -- Rules
 awful.rules.rules = {
   -- All clients will match this rule.
   {rule = { },
-  properties = { border_width = beautiful.border_width,
-  border_color = beautiful.border_normal,
-  focus = awful.client.focus.filter,
-  keys = clientkeys,
-  buttons = clientbuttons,
-  size_hints_honor = false } },
-
-  {rule = { class = 'urxvt' },
   properties = {
-    tag = tags[1][1],
-    y = 16,
-    x = 0,
-    width = screen[1].workarea.width,
-    height = screen[1].workarea.height,
+    border_width = beautiful.border_width,
+    border_color = beautiful.border_normal,
+    focus = awful.client.focus.filter,
+    keys = clientkeys,
+    buttons = clientbuttons,
+    size_hints_honor = false
   }},
 
-  {rule = { class = 'google-chrome' },
+  {rule = {class = 'urxvt', class='google-chrome'},
+  properties = {
+    y = systray,
+    x = 0,
+  }},
+
+  {rule = {class = 'urxvt'},
+  properties = {
+    tag = tags[1][1],
+    width = screen[1].workarea.width,
+    height = screen[1].workarea.height-systray,
+  }},
+
+  {rule = {class = 'google-chrome'},
   callback = function(c)
     local s, t = 1, 2
     if screen.count() >= 2 then
@@ -33,10 +40,8 @@ awful.rules.rules = {
     c:tags({tags[s][t]})
 
     c:geometry({
-      y = 16,
-      x = 0,
       width = screen[s].workarea.width,
-      height = screen[s].workarea.height,
+      height = screen[s].workarea.height-systray,
     })
-  end,},
+  end},
 }
